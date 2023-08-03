f682678eb0cc8081ba64ecc5cdd5f1d337fe0bf3
Gary Kovar <gkovar@gmail.com>
2023-08-03 00:25:46 +0000

    set end state

--- example.php
+++ example.php
@@ -6,4 +6,14 @@ function add( int $x, int $y ) :int {
   return $x+$y; 
 }
 
+function multiply( int $x, int $y ) :int {
+  // do it with the add function
+  $result = 0;
+  for( $i = 1; $i < $y; $i++ ) {
+    $result = add( $result, $x );
+  }
+
+  return $result;
+}
+
 echo add( 5, 2 );
