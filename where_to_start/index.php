1fda339de512cc62baaa16d9b6931d19db56d6e8
Gary Kovar <gkovar@gmail.com>
2023-09-23 15:18:07 -0400

    initial setup

---
+++ where_to_start/index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+// Maybe redirect.
+$redirects = [
+    'linkedin' => 'https://www.linkedin.com',
+    'learning' => 'https://www.linkedin.com/learning/',
+];
+
+// Check the REQUEST_URI to see if it matches a redirect.
+$uri = $_SERVER['REQUEST_URI'];
+$uri = trim( $uri, '/' );
+$uri = explode( '/', $uri );
+$uri = $uri[0];
+
+if( array_key_exists( $uri, $redirects ) ) {
+    header( 'Location: ' . $redirects[$uri] );
+    exit;
+}
+
+// Rest of the website
