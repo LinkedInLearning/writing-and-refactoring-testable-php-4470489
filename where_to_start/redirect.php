ec116b7a7182c09667c7e6d1dbdc582153fd126e
Gary Kovar <gkovar@gmail.com>
2023-09-23 20:08:21 -0400

    indents

--- where_to_start/redirect.php
+++ where_to_start/redirect.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 function maybe_redirect()
 {
-// Maybe redirect.
+    // Maybe redirect.
     $redirects = [
         'linkedin' => 'https://www.linkedin.com',
         'learning' => 'https://www.linkedin.com/learning/',
     ];
 
-// Check the REQUEST_URI to see if it matches a redirect.
+    // Check the REQUEST_URI to see if it matches a redirect.
     $uri = $_SERVER['REQUEST_URI'];
     $uri = trim($uri, '/');
     $uri = explode('/', $uri);
