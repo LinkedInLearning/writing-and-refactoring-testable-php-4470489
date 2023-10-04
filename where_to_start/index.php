d3ea5ae9aa84604867da5fe341c3722831a24082
Gary Kovar <gkovar@gmail.com>
2023-10-04 09:12:23 -0400

    end state

--- where_to_start/index.php
+++ where_to_start/index.php
@@ -2,6 +2,11 @@
 declare(strict_types=1);
 
 require 'redirect.php';
+require 'Redirect_Collection_Interface.php';
+require 'Redirect_Collection_Null.php';
+require 'Redirect_Collection_From_Array.php';
+require 'Redirects.php';
+require 'RedirectVO.php';
 
 maybe_redirect();
 
